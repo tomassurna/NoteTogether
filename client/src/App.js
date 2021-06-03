89333243a24f8bcb19ea7112442ded4c5a77cfd3
Fabio <famarcellus12@gmail.com>
2021-06-03 12:00:04 -0400

    Fix client folder

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import Home from "./components/home/home.js"
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/home/home.js
@@ -0,0 +1,24 @@
+import './home.scss';
+import UploadImage from "../../assets/upload.svg"
+
+function Home() {
+  return (
+    <div id="home" className="flex-container">
+        <h1 className="heading">Note Together</h1>
+        <h2 className="instructions">Upload your own video to enter a Youtube video link!</h2>
+        <div className="video-section">
+            <input className="youtube-link" placeholder="Put Youtube Link here"></input> 
+            <h1 className="or-text">OR</h1>
+            <img className="upload-icon" src={UploadImage} width="53.90" height="71.87"></img>
+        </div>
+        <h2 className="instructions">Enter a title and upload!</h2>
+        <div className="title-section">
+            <input className="video-title" placeholder="Enter Video Title here"></input>
+            <button className="upload-button">Upload</button>
+        </div>
+        
+    </div>
+  );
+}
+
+export default Home;
