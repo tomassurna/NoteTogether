01eeea0745e745719e66598d5c839792a00116e0
tomassurna <tomas.surna@hotmail.com>
2021-07-29 06:28:38 -0400

    tsurna - move around code to allow packaging for deployment

--- noteTogther/migrations/2_deploy_contract.js
+++
@@ -1,5 +0,0 @@
-var NoteTogether = artifacts.require("./NoteTogether.sol");
-
-module.exports = function (deployer, network, accounts) {
-  deployer.deploy(NoteTogether, { from: accounts[0], gas: 6700000 });
-};
